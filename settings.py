55678c6a2ba0b4a9d8acce60aba35dc2186b06c3
tomsloniewski <t.sloniewski@gmail.com>
2025-04-13 10:08:02 +0200

    Added Pydantic settings

---
+++ settings.py
@@ -0,0 +1,16 @@
+from pydantic_settings import BaseSettings
+from pydantic import validator, ValidationError
+
+
+class Settings(BaseSettings):
+    ENVIRONMENT: str
+    APP_NAME: str
+
+    @validator("environment")
+    def validate_environment(cls, value):
+        envs = {"dev", "test", "prod"}
+        if value not in envs:
+            raise ValidationError(
+                f"Provided wrong environment: {value}. List of allowed environments: {envs}."
+            )
+        return value
